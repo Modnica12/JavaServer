573463ebba122e4c9f79d94a854ce57fd028b5d3
Modnica12 <jcycu12@gmail.com>
2019-12-19 13:44:00 +0500

    Auth service in other project AuthServer

--- src/main/java/com/faceless/handlers/AuthHandler.java
+++ src/main/java/com/faceless/handlers/AuthHandler.java
@@ -8,6 +8,8 @@ import com.faceless.responses.Response;
 import com.google.gson.JsonPrimitive;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginHandler extends RequestHandler
 {
@@ -19,11 +21,34 @@ public class LoginHandler extends RequestHandler
 
 		String login    = request.getArgumentValue("login");
 		int password = request.getArgumentValue("password").hashCode();
-		String stmt =
-				"INSERT IGNORE INTO users(login, password)\n" +
-						"   VALUE ('" + login + "','" + password + "')\n"+
-						"ON DUPLICATE KEY UPDATE login=login";
-		Application.server.database.executeUpdate(stmt);
+		String getPassword = "SELECT password from users WHERE login = '" + login + "';";
+
+		ResultSet dbPassword = Application.server.database.executeQuery(getPassword);
+
+		try {
+			String thisPassword = null;
+			if (!dbPassword.next()) {
+				String stmt =
+						"INSERT IGNORE INTO users(login, password)\n" +
+								"   VALUE ('" + login + "','" + password + "')\n"+
+								"ON DUPLICATE KEY UPDATE login=login";
+				Application.server.database.executeUpdate(stmt);
+			}
+			else{
+				thisPassword = dbPassword.getString("password");
+				if (thisPassword.hashCode() != password){
+					response.setStatus("401");
+					response.setDescription("Incorrect password");
+					return;
+				}
+			}
+
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+
 		propertyContainer.setProperty("logged_in", "true");
 		propertyContainer.setProperty("login", login);
 		propertyContainer.setProperty("password", password);
